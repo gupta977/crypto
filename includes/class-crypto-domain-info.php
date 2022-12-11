01324c4ee217ba24e03717c144236c6e5208d40a
gupta977 <navneet@odude.com>
2022-12-11 18:26:14 +0545

    info

--- includes/class-crypto-domain-info.php
+++ includes/class-crypto-domain-info.php
@@ -1,36 +1,55 @@
 <?php
 class Crypto_Domain_INFO
 {
-	private $market_page;
-	private $search_page;
-	private $url_page;
-	private $price_ether;
+    private $market_page;
+    private $search_page;
+    private $url_page;
+    private $price_ether;
 
-	public function __construct()
-	{
+    public function __construct()
+    {
 
-		add_shortcode('crypto-domain-info', array($this, 'start'));
-		$this->search_page = crypto_get_option('search_page', 'crypto_marketplace_settings', 0);
-		$this->market_page = crypto_get_option('market_page', 'crypto_marketplace_settings', 0);
-		$this->url_page = crypto_get_option('url_page', 'crypto_marketplace_settings', 0);
-		$this->price_ether = crypto_get_option('price_ether', 'crypto_marketplace_settings', '5');
-	}
+        add_shortcode('crypto-domain-info', array($this, 'start'));
+        $this->search_page = crypto_get_option('search_page', 'crypto_marketplace_settings', 0);
+        $this->market_page = crypto_get_option('market_page', 'crypto_marketplace_settings', 0);
+        $this->url_page = crypto_get_option('url_page', 'crypto_marketplace_settings', 0);
+        $this->price_ether = crypto_get_option('price_ether', 'crypto_marketplace_settings', '5');
+    }
 
-	public function start()
-	{
-		ob_start();
-		global $wp_query;
-		if (0 != $this->search_page) {
-			$this->search_page = esc_url(get_page_link($this->search_page));
-		} else {
-			$this->search_page = "#";
-		}
-		if (0 != $this->market_page) {
-			$this->market_page = esc_url(get_page_link($this->market_page));
-		} else {
-			$this->market_page = "#";
-		}
+    public function start()
+    {
+        ob_start();
+        global $wp_query;
+        if (0 != $this->search_page) {
+            $this->search_page = esc_url(get_page_link($this->search_page));
+        } else {
+            $this->search_page = "#";
+        }
+        if (0 != $this->market_page) {
+            $this->market_page = esc_url(get_page_link($this->market_page));
+        } else {
+            $this->market_page = "#";
+        }
+
+
+        if (isset($_GET['domain'])) {
+            // echo $_GET['domain'];
 ?>
+
+<script>
+jQuery(document).ready(function() {
+    var final_domain = "<?php echo $_GET['domain']; ?>";
+
+    jQuery("[id=crypto_domain_name]").html(final_domain);
+
+    jQuery("#crypto_manage_domain").attr("href",
+        "<?php echo get_site_url(); ?>/web3/" + final_domain +
+        "/?domain=manage");
+    jQuery("#crypto_ipfs_domain").attr("href",
+        "<?php echo get_site_url(); ?>/web3/" + final_domain +
+        "/");
+});
+</script>
 <div class="fl-columns">
     <div class="fl-column fl-is-three-quarters">
 
@@ -58,38 +77,32 @@ class Crypto_Domain_INFO
             </div>
 
 
-            <?php
-
-					if (isset($_GET['domain'])) {
-						echo $_GET['domain'];
-					} else {
-						echo "No domain";
-					}
-					?>
-
             <div id="crypto_loading_url" style="text-align:center;"> Redirection on progress..
                 <br>
                 <a href="#" id="crypto_loading_url_link">External Link</a>
             </div>
-            <article class="fl-message fl-is-danger" id="crypto_unavailable">
-                <div class="fl-message-body">
-                    <div class="fl-tags fl-has-addons">
-                        <span class="fl-tag fl-is-large" id="crypto_domain_name">Domain Name</span>
-                        <span class="fl-tag fl-is-danger fl-is-large" id="crypto_domain_name_error">Unavailable</span>
-                    </div>
-                </div>
-            </article>
+
 
             <div id="json_container"></div>
 
         </div>
 
     </div>
+    <footer class="fl-card-footer">
+        <a href="#" class="fl-card-footer-item" id="crypto_blockchain_url">Blockchain Record</a>
+        <a href="#" class="fl-card-footer-item" id="crypto_manage_domain">Manage Domain</a>
+        <a href="<?php echo $this->url_page; ?>" target="_blank" class="fl-card-footer-item"
+            id="crypto_ipfs_domain">Visit Site</a>
+    </footer>
 </div>
-<?PHP
+<?php
+        } else {
+            echo "No domain";
+        }
 
-		$content = ob_get_clean();
-		return $content;
-	}
+
+        $content = ob_get_clean();
+        return $content;
+    }
 }
 new Crypto_Domain_INFO();
